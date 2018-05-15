13d5935ca809237bf2574f7fdd4cd7886766c27c
Xirasno <broekman@live.nl>
2018-05-15 11:23:24 +0200

    .asdwqd

---
+++ PointLight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace template
+{
+    class PointLight
+    {
+        public Vector3 origin, colour;
+        public float brightness;
+
+        public PointLight(Vector3 origin, float brightness, Vector3 colour)
+        {
+            this.origin = origin;
+            this.brightness = brightness;
+            this.colour = colour;
+        }
+    }
+}
