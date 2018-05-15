76610d21c574351ddf037e8bd81d474716dae684
Xirasno <broekman@live.nl>
2018-05-15 16:57:34 +0200

    Proper FOV Calculation

--- Camera.cs
+++ Camera.cs
@@ -18,17 +18,13 @@ namespace template
         public Camera()
         {
             origin = Vector3.Zero;
-            fovDist = 1f;
+            fov = 90;
+            fovDist = 1 / (float)Math.Tan(((fov * (Math.PI / 180.0f)) / 2));
             direction = new Vector3(0, 0, -1);
             centre = origin + fovDist * direction;
             screen = new Plane(centre + new Vector3(-1, -1, 0), centre + new Vector3(1, -1, 0), centre + new Vector3(-1, 1, 0), Vector3.Zero);
             pixels = new Ray[512, 512];
             PrimaryRays();
-
-            //FOV is calculated by calculating the dot product of the two vector going to the top-left and top-right corner
-            //followed by taking the ArcCos of it to find the angle. The direction is subtracted from the vectors to make sure we get the
-            //right angle, and the result is divided by pi and multiplied by 180 to get degrees instead of radians.
-            fov = ((float)Math.Round(Math.Acos(Vector3.Dot(((screen.p1 - origin) - direction), ((screen.p2 - origin) - direction))) / Math.PI * 180.0f));
         }
 
         public void PrimaryRays()
